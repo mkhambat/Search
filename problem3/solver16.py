127238b7fc169a177a0d78e5cd5622d7dce96a07
Yingnan Ju <bunny_gg@hotmail.com>
2017-09-24 13:03:10 -0400

    minor changes

--- problem3/solver16.py
+++ problem3/solver16.py
@@ -7,13 +7,10 @@ import sys
 import copy
 import time
 
+# board_end = [['1', '2'], ['3', '0']]
 # board_end = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '0']]
-
-
 board_end = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['13', '14', '15', '0']]
 
-
-# board_end = [['1', '2'], ['3', '0']]
 
 # definition of Status
 # board + Parameters
